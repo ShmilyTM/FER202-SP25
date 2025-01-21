59f8d7d4e74c658a5bff4141e9a43e11fee50409
ShmilyTM <lethienacqt@gmail.com>
2025-01-21 16:31:59 +0700

    update doc

--- lab1/src/components/App.js
+++ lab1/src/components/App.js
@@ -4,19 +4,19 @@ import HelloWorld from "./HelloWorld";
 import Person from "./Person";
 
 class App extends Component {
-  render() {
-    // Tạo đối tượng Person
-    const person = new Person("John Doe", 25);
-    return (
-      <div>
-        <h1>My React App!</h1>
-        <h2>
-          <HelloWorld />
-        </h2>
-        <h2>{person.sayHello()}</h2>
-      </div>
-    );
-  }
+ render() {
+ // Tạo đối tượng Person
+ const person = new Person("John Doe", 25);
+ return (
+ <div>
+ <h1>My React App!</h1>
+ <h2>
+ <HelloWorld />
+ </h2>
+ <h2>{person.sayHello()}</h2>
+</div>
+);
+ }
 }
 
 export default App;
